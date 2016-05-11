a363205d69e40628cc5d6621e6f97539e99ca790
Dmitry Berezovsky <dmitry.berezovsky@logicify.com>
2016-05-11 23:38:34 +0300

    renamed conf. file

--- start-nginx.sh
+++ start-nginx.sh
@@ -3,8 +3,8 @@
 cat > /etc/nginx/nginx_params <<-EOF
 proxy_send_timeout $PROXY_TIMEOUT;
 proxy_read_timeout $WSGI_PORT;
-set \$wsgi_host '$WSGI_PORT';
-set \$wsgi_port $WSGI_HOST;
+set \$wsgi_host '$WSGI_HOST';
+set \$wsgi_port $WSGI_PORT;
 EOF
 
 nginx -g 'daemon off;'
